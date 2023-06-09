299bd64d17707ccec356881839f32b8f22bbc809
Lilikukan <chenlianlan@hotmail.ca>
2023-06-08 23:11:39 -0700

    0608 updated by lian
    I have problem to make the last section's background to black and delete the line between 1-2-3. You will also need to fix the grey strip in between hero banner and feature section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,66 +1,70 @@
-$("#slides").owlCarousel({
-  loop: true,
-  margin: 10,
+$('#slides').owlCarousel({
+  loop:true,
+  margin:10,
   autoplay: true,
   smartSpeed: 1500,
-  nav: false,
-  dots: true,
-  responsive: {
-    0: {
-      items: 1,
-    },
-    600: {
-      items: 1,
-    },
-    1000: {
-      items: 1,
-    },
-  },
-});
+  nav:false,
+  dots: true, 
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:1
+      },
+      1000:{
+          items:1
+      }
+  }
+})
 
 // Fix Header Js
-$(window).scroll(function () {
-  if ($(window).scrollTop() >= 250) {
-    $("header").addClass("fix_style");
-  } else {
-    $("header").removeClass("fix_style");
-  }
-  if ($(window).scrollTop() >= 260) {
-    $("header").addClass("fixed");
-  } else {
-    $("header").removeClass("fixed");
-  }
-});
-
-function updateCount() {
-  var count = $("#todos li:not(.done)").length;
-  $("#countSpan").text(count);
-}
-
-updateCount();
-
-$("#new-todo").keypress(function (event) {
-  var keycode = event.keyCode ? event.keyCode : event.which;
-  if (keycode === 13) {
-    console.log("pressed");
-    if ($(this).val().length !== 0) {
-      var toDoCount = $("#todos li").length + 1;
-      $("#todos").prepend(
-        '<li><input id="checkbox-' +
-          toDoCount +
-          '" type="checkbox"><label for="checkbox-' +
-          toDoCount +
-          '">' +
-          $(this).val() +
-          '<span class="box"></span></label></li>'
-      );
-      $(this).val("");
-      updateCount();
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 250) {
+        $('header').addClass('fix_style');
     }
-  }
+    else {
+        $('header').removeClass('fix_style');
+    }
+    if ($(window).scrollTop() >= 260) {
+        $('header').addClass('fixed');
+    }
+    else {
+        $('header').removeClass('fixed');
+    }
+  });
+  
+
+
+  $("#new-todo").keypress(function (event) {
+	var keycode = event.keyCode ? event.keyCode : event.which;
+	if (keycode == "13") {
+		console.log("pressed");
+
+		if ($(this).val().length !== 0) {
+			var toDoCount = $("span.box").length + 1;
+
+			$("#todos").prepend(
+				'<li><input id="checkbox-' +
+					toDoCount +
+					'" type="checkbox"><label for="checkbox-' +
+					toDoCount +
+					'">' +
+					$(this).val() +
+					'<span class="box"></span></label></li>'
+			);
+			$(this).val("");
+		}
+	}
 });
+
 
 $("#todos").on("click", "label", function () {
-  $(this).closest("li").toggleClass("done");
-  updateCount();
+	$(this).closest("li").toggleClass("done");
 });
+
+gsap.registerPlugin(EasePack);
+
+const tl = gsap.timeline();
+var master = new TimelineMax();
+

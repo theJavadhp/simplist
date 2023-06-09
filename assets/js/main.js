97498a939bea1fd6627850aa6f327463a8010b7e
theJavadhp <mjhp29@yahoo.com>
2023-06-09 12:04:19 -0400

    Revert "Merge pull request #1 from Lilikukan/main"
    This reverts commit ea3bb913fc3f68f2df7956f677bbc42613e4aac0, reversing
    changes made to 75edeb78a0731e92a3d9a90b5dd4ef0d5ab360a2.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,70 +1,66 @@
-$('#slides').owlCarousel({
-  loop:true,
-  margin:10,
+$("#slides").owlCarousel({
+  loop: true,
+  margin: 10,
   autoplay: true,
   smartSpeed: 1500,
-  nav:false,
-  dots: true, 
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:1
-      },
-      1000:{
-          items:1
-      }
-  }
-})
+  nav: false,
+  dots: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
 
 // Fix Header Js
-$(window).scroll(function(){
-    if ($(window).scrollTop() >= 250) {
-        $('header').addClass('fix_style');
-    }
-    else {
-        $('header').removeClass('fix_style');
-    }
-    if ($(window).scrollTop() >= 260) {
-        $('header').addClass('fixed');
-    }
-    else {
-        $('header').removeClass('fixed');
-    }
-  });
-  
-
-
-  $("#new-todo").keypress(function (event) {
-	var keycode = event.keyCode ? event.keyCode : event.which;
-	if (keycode == "13") {
-		console.log("pressed");
-
-		if ($(this).val().length !== 0) {
-			var toDoCount = $("span.box").length + 1;
-
-			$("#todos").prepend(
-				'<li><input id="checkbox-' +
-					toDoCount +
-					'" type="checkbox"><label for="checkbox-' +
-					toDoCount +
-					'">' +
-					$(this).val() +
-					'<span class="box"></span></label></li>'
-			);
-			$(this).val("");
-		}
-	}
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= 250) {
+    $("header").addClass("fix_style");
+  } else {
+    $("header").removeClass("fix_style");
+  }
+  if ($(window).scrollTop() >= 260) {
+    $("header").addClass("fixed");
+  } else {
+    $("header").removeClass("fixed");
+  }
 });
 
+function updateCount() {
+  var count = $("#todos li:not(.done)").length;
+  $("#countSpan").text(count);
+}
+
+updateCount();
+
+$("#new-todo").keypress(function (event) {
+  var keycode = event.keyCode ? event.keyCode : event.which;
+  if (keycode === 13) {
+    console.log("pressed");
+    if ($(this).val().length !== 0) {
+      var toDoCount = $("#todos li").length + 1;
+      $("#todos").prepend(
+        '<li><input id="checkbox-' +
+          toDoCount +
+          '" type="checkbox"><label for="checkbox-' +
+          toDoCount +
+          '">' +
+          $(this).val() +
+          '<span class="box"></span></label></li>'
+      );
+      $(this).val("");
+      updateCount();
+    }
+  }
+});
 
 $("#todos").on("click", "label", function () {
-	$(this).closest("li").toggleClass("done");
+  $(this).closest("li").toggleClass("done");
+  updateCount();
 });
-
-gsap.registerPlugin(EasePack);
-
-const tl = gsap.timeline();
-var master = new TimelineMax();
-
